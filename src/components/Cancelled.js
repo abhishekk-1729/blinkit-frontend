4dcf0805c79dc8bc5a48699ae52633e9b731fcf1
Abhishek Kumar <abhishekkumar@Abhisheks-Air-2.domain.name>
2024-06-02 18:59:20 +0530

    new

--- src/components/Cancelled.js
+++ src/components/Cancelled.js
@@ -3,17 +3,7 @@ import React from 'react'
 export default function Cancelled() {
   return (
     <div>
-      hi
-      hi
-      hi
-      hi
-      hi
-      hi
-      hi
-      hi
-      hi
-      hi
-      hi
+      Unsuccessful
     </div>
   )
 }
